29da1820a2e42304a68deef3fea9da83f8744218
crpalomo <crpalomo@outlook.com>
2018-02-20 00:25:37 -0800

    Uploading Part 3
    Not sure if this is correct, I adjusted  the inital values and found a pattern that was not just a filled graph.

---
+++ Part3.m
@@ -0,0 +1,52 @@
+c=1.25;
+phi = @(z) sqrt(z - c);
+
+colormap([1 0 0; 1 1 1]);
+
+M = 2*ones(141,361);
+
+for j=1:141
+    y = -0.01 + (j-1)*0.1;
+    for i =1:361
+        x = -0.01 + (i-1)*0.1;
+        
+        r = sqrt(x^2 + y^2);
+        theta = atan(y/x);
+        z = r*(cos(theta-x) + sin(theta-y));
+        
+        fixpt1 = sqrt(r*cos((theta-x)/2))+sqrt(r*sin((theta-y)/2));
+        fixpt2 = -sqrt(r*cos((theta-x)/2))-sqrt(r*sin((theta-y)/2));
+        zk = phi(z);
+        zl = -phi(z);
+        iflag1 = 0;
+        iflag2 = 0;
+        kcount = 0;
+        
+        while kcount < 100 && abs(zk) < 2 && iflag1 < 5 && iflag2 < 5
+            kcount = kcount + 1;
+            
+            err1 = abs(zk-fixpt1);
+            err3 = abs(zl-fixpt1);
+                if err1 < 1.e-6 && err3 < 1.e-6
+                    iflag1 = iflag1 +1; 
+                else
+                    iflag1 = 0;
+                end
+                
+            err2 = abs(zk - fixpt2);
+            err4 = abs(zl - fixpt2);
+                if err2 < 1.e-6 && err4 < 1.e-6
+                    iflag2 = iflag2 +1; 
+                else
+                    iflag2 = 0;
+                end
+        end
+        
+        
+        if iflag1 >= 5 || iflag2 >= 5 || kcount >= 100
+            M(j,i) = 1;
+        end
+    end
+end
+image([-1 1],[-1 1],M),
+axis xy
